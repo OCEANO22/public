98d41b45012728ad55dd651ff6c8cbb215465588
OCEANO22 <n.oceano22@gmail.com>
2025-06-12 15:59:53 +0700

    setting cloudflare

--- api/index.js
+++ api/index.js
@@ -1,22 +1,35 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url)
+import { Hono } from 'hono';
 
-    if (request.method === 'POST' && url.pathname === '/api/komentar') {
-      try {
-        const body = await request.json()
-        console.log("Data masuk:", body)
+const app = new Hono();
 
-        await env.DB.prepare(
-          `INSERT INTO komentar (nama, email, pesan) VALUES (?, ?, ?)`
-        ).bind(body.nama, body.email, body.pesan).run()
+app.get('/api', (c) => {
+  return c.text('hi');
+})
 
-        return new Response("Komentar berhasil disimpan", { status: 200 })
-      } catch (err) {
-        console.error("Gagal menyimpan komentar:", err)
-        return new Response("Terjadi kesalahan server", { status: 500 })
-      }
-    }
+app.get('/api/komentar', async (c) => {
+  let { results } = await c.env.DB.prepare("SELECT * FROM komentar").all()
+  return c.json(results)
+})
 
-    return new Response("Not Found", { status: 404 })
-  }}
+app.get('*', (c) => c.env.ASSETS.fetch(c.req.raw));
+
+export default app;
+
+    //if (request.method === 'POST' && url.pathname === '/api/komentar') {
+      //try {
+        //const body = await request.json()
+        //console.log("Data masuk:", body)
+
+        //await env.DB.prepare(
+          //`INSERT INTO komentar (nama, email, pesan) VALUES (?, ?, ?)`
+        //).bind(body.nama, body.email, body.pesan).run()
+
+        //return new Response("Komentar berhasil disimpan", { status: 200 })
+      //} catch (err) {
+        //console.error("Gagal menyimpan komentar:", err)
+        //return new Response("Terjadi kesalahan server", { status: 500 })
+      //}
+    //}
+
+    //return new Response("Not Found", { status: 404 })
+  //}}
