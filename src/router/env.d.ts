b739449f2890f1adc5a5459cc08fe30c5a158fbe
OCEANO22 <n.oceano22@gmail.com>
2025-06-22 08:33:21 +0700

    env.d.ts

--- src/router/env.d.ts
+++ src/router/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  BASE_URL: string
+  // add other environment variables here if needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
